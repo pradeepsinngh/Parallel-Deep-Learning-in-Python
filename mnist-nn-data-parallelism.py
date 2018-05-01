e157fbe6ac046bb9f1bf95cf2f16b5badf783d15
Pradeep Singh <pdeepsingh91@gmail.com>
2018-05-01 12:08:41 -0700

    Update mnist-nn-data-parallelism.py

--- mnist-nn-data-parallelism.py
+++ mnist-nn-data-parallelism.py
@@ -8,19 +8,6 @@
 # To distribute data to worknodes, MPI (mpi4py) is used.  To execute
 # matrix operations, Theano is used.
 #
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import functools
 import numpy as np
