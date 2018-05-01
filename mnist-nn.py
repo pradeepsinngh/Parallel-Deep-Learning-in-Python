21fc25bfcbb08471bdefccf462b9412491007103
Pradeep Singh <pdeepsingh91@gmail.com>
2018-05-01 12:09:06 -0700

    Update mnist-nn.py

--- mnist-nn.py
+++ mnist-nn.py
@@ -2,31 +2,10 @@
 #
 # MNIST digit recognizer
 #
-# The simple recognizer is implememted purely in Python.  The purpose of
-# this program is to present the details how to constructa simple neural
-# network for prediction from scratch:
-#
 # 1. To build a 3-layer neural network (only one hidden layer).
 # 2. To train a model with self-implemented SGD (stochastic gradient descent).
 # 3. To predict data with the trained model.
 #
-# This program is based on the exercise of Andrew Ng's machine learning
-# course on Coursera: https://www.coursera.org/learn/machine-learning
-#
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 import numpy as np
 import math
